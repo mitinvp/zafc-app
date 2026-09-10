b2d508408c930a8acfd8c13e7e751ff5808bda2d
mitinvp <mitin@zac.org.ua>
2026-09-10 10:00:41 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'zac-app-v4';
+const CACHE = 'zac-app-v5';
 const ASSETS = ['./', './index.html', './schedule.html', './bells.html', './manifest.json', './icons/icon-192.png', './icons/icon-512.png'];
 
 self.addEventListener('install', (event) => {
